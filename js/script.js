36cc58a811dea0604cfe78282021263227d720fc
shukannya <112769948+shukannya@users.noreply.github.com>
2022-10-24 10:02:00 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,47 @@
+let userBox = document.querySelector('.header .header-2 .user-box');
+
+document.querySelector('#user-btn').onclick = () =>{
+   userBox.classList.toggle('active');
+   navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.header .header-2 .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+   navbar.classList.toggle('active');
+   userBox.classList.remove('active');
+}
+window.onscroll = () =>{
+   userBox.classList.remove('active');
+   navbar.classList.remove('active');
+
+   if(window.scrollY > 60){
+      document.querySelector('.header .header-2').classList.add('active');
+   }else{
+      document.querySelector('.header .header-2').classList.remove('active');
+   }
+}
+
+
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+
+
+
