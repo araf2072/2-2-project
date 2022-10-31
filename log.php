9c33d4939e743441ba2d8defbdea02fa5c138466
Md Sazzad Hossain Bhuiyan <111766115+sazzad010@users.noreply.github.com>
2022-10-31 12:07:13 +0600

    Update log.php

--- log.php
+++ log.php
@@ -44,3 +44,25 @@ if(isset($_POST['btn']))
 }
 
 ?>
+
+<!doctype html>
+<html lang="en" class="fixed accounts sign-in">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <title>Login page</title>
+
+        <!--BASIC css-->
+        <!-- ========================================================= -->
+        <link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.css">
+        <link rel="stylesheet" href="assets/vendor/font-awesome/css/font-awesome.css">
+        <link rel="stylesheet" href="assets/vendor/animate.css/animate.css">
+        <!--SECTION css-->
+        <!-- ========================================================= -->
+        <!--TEMPLATE css-->
+        <!-- ========================================================= -->
+        <link rel="stylesheet" href="assets/stylesheets/css/style.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    
